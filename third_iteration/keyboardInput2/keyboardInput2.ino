cd832fd92ffa37eba8f5e4820804dc9ac197777a
SelinaW <selina@audiocommon.com>
2015-11-16 02:11:47 -0500

    Half-finished iteration 3 code

--- third_iteration/keyboardInput2/keyboardInput2.ino
+++ third_iteration/keyboardInput2/keyboardInput2.ino
@@ -9,21 +9,21 @@ Control Modes: Type in the mode you want (0~4) in the Serial Monitor
 #include "utility/Adafruit_PWMServoDriver.h"
 
 // Create the motor shield object with the default I2C address
-Adafruit_MotorShield AFMS = Adafruit_MotorShield();
+// Adafruit_MotorShield AFMS = Adafruit_MotorShield();
 // Or, create it with a different I2C address (say for stacking)
-// Adafruit_MotorShield AFMS = Adafruit_MotorShield(0x61);
+Adafruit_MotorShield AFMS = Adafruit_MotorShield(0x61);
 
 // Initiate the motors controlling the left and right side of the body
-Adafruit_DCMotor *lefthandMotor = AFMS.getMotor(0x60);
-Adafruit_DCMotor *righthandMotor = AFMS.getMotor(0x61);
-Adafruit_DCMotor *leftfootMotor = AFMS.getMotor(0x62);
-Adafruit_DCMotor *rightfootMotor = AFMS.getMotor(0x63);
-Adafruit_DCMotor *laterallefthandMotor = AFMS.getMotor(0x64);
-Adafruit_DCMotor *lateralrighthandMotor = AFMS.getMotor(0x65);
-Adafruit_DCMotor *leftheadMotor = AFMS.getMotor(0x66);
-Adafruit_DCMotor *rightheadMotor = AFMS.getMotor(0x67);
-Adafruit_DCMotor *shoulderMotor = AFMS.getMotor(0x68);
-Adafruit_DCMotor *bottomMotor = AFMS.getMotor(0x70);
+Adafruit_DCMotor *lefthandMotor = AFMS.getMotor(1);
+Adafruit_DCMotor *righthandMotor = AFMS.getMotor(2);
+Adafruit_DCMotor *leftfootMotor = AFMS.getMotor(3);
+Adafruit_DCMotor *rightfootMotor = AFMS.getMotor(4);
+Adafruit_DCMotor *laterallefthandMotor = AFMS.getMotor(5);
+Adafruit_DCMotor *lateralrighthandMotor = AFMS.getMotor(6);
+Adafruit_DCMotor *leftheadMotor = AFMS.getMotor(7);
+Adafruit_DCMotor *rightheadMotor = AFMS.getMotor(8);
+Adafruit_DCMotor *shoulderMotor = AFMS.getMotor(9);
+Adafruit_DCMotor *bottomMotor = AFMS.getMotor(10);
 
 int mode = 0; // 
 unsigned long currentMillis = 0; // Millis used to determine how long each motion is
@@ -34,9 +34,23 @@ int mode1counter = 0;
 int mode2counter = 0;
 int mode3counter = 0;
 int mode4counter = 0;
+const int buttonPin1 = 8;    
+const int buttonPin2 = 9;
+const int buttonPin3 = 10;
+const int buttonPin4 = 11;
+const int buttonPin5 = 12;
+int currButtonState; // start off our variables - this is saying WHAT IS HAPPENING WITH THE BUTTON NOW (high/low)
+int preButtonState; //
+long lastTimeDebounce = 0; // when did we last change the outputs
+long debounceDelay = 100; // let's make this a decent delay
 int keyboardInput;
 
 void setup() {
+    pinMode(buttonPin1, INPUT);
+    pinMode(buttonPin2, INPUT);
+    pinMode(buttonPin3, INPUT);
+    pinMode(buttonPin4, INPUT);
+    pinMode(buttonPin5, INPUT);
     AFMS.begin();  // create with the default frequency 1.6KHz
     // Set the speed to start, from 0 (off) to 255 (max speed)
     lefthandMotor->setSpeed(150);
@@ -64,15 +78,48 @@ void loop() {
     }
   }
    
-      if (mode != preMode) { // if the mode has changed
-        preMode = mode; // then we'd like to update our mode
-        mode0counter = 0;
-        mode1counter = 0;
-        mode2counter = 0;
-        mode3counter = 0;
-        mode4counter = 0;
+  if (mode != preMode) { // if the mode has changed
+    preMode = mode; // then we'd like to update our mode
+    mode0counter = 0;
+    mode1counter = 0;
+    mode2counter = 0;
+    mode3counter = 0;
+    mode4counter = 0;
+  }
+
+  int buttonRead1 = digitalRead(buttonPin1); // what are the buttons doing RIGHT NOW
+  int buttonRead2 = digitalRead(buttonPin2);
+  int buttonRead3 = digitalRead(buttonPin3);
+  int buttonRead4 = digitalRead(buttonPin4);
+  int buttonRead5 = digitalRead(buttonPin5);
+
+
+  if (buttonRead1 != preButtonState || buttonRead2 != preButtonState|| buttonRead3 != preButtonState|| buttonRead4 != preButtonState || buttonRead5 != preButtonState) { // if the button looks pressed, then
+     // reset the debouncing timer
+     lastTimeDebounce = millis();
       }
-  
+  if ((millis() - lastTimeDebounce) > debounceDelay) { // if the time from last press has been enough
+   
+        if (buttonRead1 != currButtonState || buttonRead2 != currButtonState || buttonRead3 != currButtonState|| buttonRead4 != currButtonState|| buttonRead5 != currButtonState) { // and if what the button is saying now is not equal to the current reading
+        currButtonState = buttonRead; // then we'd like to update our button state again
+
+ 
+            if (currButtonState == HIGH) {
+                mode++; // if the button is PRESSED NOW, then we would like to CHANGE MODES
+                mode0counter = 0;
+                mode1counter = 0;
+                mode2counter = 0;
+                mode3counter = 0;
+                mode4counter = 0;
+                if (mode == 5) {
+                  mode = 0; // reset the mode to zero if we go past our 5th mode
+                }
+            }
+        }
+     }
+  preButtonState = buttonRead; // so our loops behaves itself
+
+      
   if (mode == 0) {  // Right hand wave
       if (mode0counter == 0) {
         currentMillis = millis();
@@ -423,9 +470,9 @@ void loop() {
     lateralrighthandMotor->run(RELEASE);
     laterallefthandMotor->run(RELEASE);
     leftheadMotor->run(RELEASE);
-      rightheadMotor->run(RELEASE);
-      shoulderMotor->run(RELEASE);
-      bottomMotor->run(RELEASE);
+    rightheadMotor->run(RELEASE);
+    shoulderMotor->run(RELEASE);
+    bottomMotor->run(RELEASE);
     }
     }
     if (mode2counter == 0) {
@@ -440,9 +487,9 @@ void loop() {
     lateralrighthandMotor->run(RELEASE);
     laterallefthandMotor->run(RELEASE);
     leftheadMotor->run(RELEASE);
-      rightheadMotor->run(RELEASE);
-      shoulderMotor->run(RELEASE);
-      bottomMotor->run(RELEASE);
+    rightheadMotor->run(RELEASE);
+    shoulderMotor->run(RELEASE);
+    bottomMotor->run(RELEASE);
     }
     }
     if (mode2counter == 1) {
@@ -457,9 +504,9 @@ void loop() {
     lateralrighthandMotor->run(RELEASE);
     laterallefthandMotor->run(RELEASE);
     leftheadMotor->run(RELEASE);
-      rightheadMotor->run(RELEASE);
-      shoulderMotor->run(RELEASE);
-      bottomMotor->run(RELEASE);
+    rightheadMotor->run(RELEASE);
+    shoulderMotor->run(RELEASE);
+    bottomMotor->run(RELEASE);
     }
     }
   }
@@ -477,9 +524,9 @@ void loop() {
     lateralrighthandMotor->run(RELEASE);
     laterallefthandMotor->run(RELEASE);
     leftheadMotor->run(RELEASE);
-      rightheadMotor->run(RELEASE);
-      shoulderMotor->run(RELEASE);
-      bottomMotor->run(RELEASE);
+    rightheadMotor->run(RELEASE);
+    shoulderMotor->run(RELEASE);
+    bottomMotor->run(RELEASE);
     }
     }
     if (mode3counter == 1) {
@@ -494,9 +541,9 @@ void loop() {
     lateralrighthandMotor->run(RELEASE);
     laterallefthandMotor->run(RELEASE);
     leftheadMotor->run(RELEASE);
-      rightheadMotor->run(RELEASE);
-      shoulderMotor->run(RELEASE);
-      bottomMotor->run(RELEASE);
+    rightheadMotor->run(RELEASE);
+    shoulderMotor->run(RELEASE);
+    bottomMotor->run(RELEASE);
     }
     }
     if (mode3counter == 2) {
@@ -511,9 +558,9 @@ void loop() {
     lateralrighthandMotor->run(RELEASE);
     laterallefthandMotor->run(RELEASE);
     leftheadMotor->run(RELEASE);
-      rightheadMotor->run(RELEASE);
-      shoulderMotor->run(RELEASE);
-      bottomMotor->run(RELEASE);
+    rightheadMotor->run(RELEASE);
+    shoulderMotor->run(RELEASE);
+    bottomMotor->run(RELEASE);
     }
     }
     if (mode3counter == 3) {
@@ -528,9 +575,9 @@ void loop() {
     lateralrighthandMotor->run(RELEASE);
     laterallefthandMotor->run(RELEASE);
     leftheadMotor->run(RELEASE);
-      rightheadMotor->run(RELEASE);
-      shoulderMotor->run(RELEASE);
-      bottomMotor->run(RELEASE);
+    rightheadMotor->run(RELEASE);
+    shoulderMotor->run(RELEASE);
+    bottomMotor->run(RELEASE);
     }
     }
   }
@@ -549,9 +596,9 @@ void loop() {
     lateralrighthandMotor->run(RELEASE);
     laterallefthandMotor->run(RELEASE);
     leftheadMotor->run(RELEASE);
-      rightheadMotor->run(RELEASE);
-      shoulderMotor->run(RELEASE);
-      bottomMotor->run(RELEASE);
+    rightheadMotor->run(RELEASE);
+    shoulderMotor->run(RELEASE);
+    bottomMotor->run(RELEASE);
     }
     }
     if (mode4counter == 1) {
@@ -566,9 +613,9 @@ void loop() {
     lateralrighthandMotor->run(RELEASE);
     laterallefthandMotor->run(RELEASE);
     leftheadMotor->run(RELEASE);
-      rightheadMotor->run(RELEASE);
-      shoulderMotor->run(RELEASE);
-      bottomMotor->run(RELEASE);
+    rightheadMotor->run(RELEASE);
+    shoulderMotor->run(RELEASE);
+    bottomMotor->run(RELEASE);
     }
     }
     if (mode4counter == 2) {
@@ -583,9 +630,9 @@ void loop() {
     lateralrighthandMotor->run(RELEASE);
     laterallefthandMotor->run(RELEASE);
     leftheadMotor->run(RELEASE);
-      rightheadMotor->run(RELEASE);
-      shoulderMotor->run(RELEASE);
-      bottomMotor->run(RELEASE);
+    rightheadMotor->run(RELEASE);
+    shoulderMotor->run(RELEASE);
+    bottomMotor->run(RELEASE);
     }
     }
     if (mode4counter == 3) {
@@ -600,9 +647,9 @@ void loop() {
     lateralrighthandMotor->run(RELEASE);
     laterallefthandMotor->run(RELEASE);
     leftheadMotor->run(RELEASE);
-      rightheadMotor->run(RELEASE);
-      shoulderMotor->run(RELEASE);
-      bottomMotor->run(RELEASE);
+    rightheadMotor->run(RELEASE);
+    shoulderMotor->run(RELEASE);
+    bottomMotor->run(RELEASE);
     }
     }
   }
